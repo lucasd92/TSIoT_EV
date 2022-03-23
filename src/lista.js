3b5575d5b931008ba209d7d591f94cae42416e03
unknown <lucasd92@hotmail.com>
2022-03-22 22:44:28 -0300

    Added remove function

--- src/lista.js
+++ src/lista.js
@@ -1,41 +1,37 @@
 module.exports = class List {
-    elements
-    constructor(){
-        this.elements = []
-    }
+  elements
+  constructor(){
+    this.elements = []
+  }
     
-    count() {
-        return this.elements.length
+  count() {
+    return this.elements.length
+  }
+  get_index(key){
+    return this.elements.findIndex((element)=> element.key == key ) 
+  }
+  find(key) {
+    var index
+    if (this.elements.length == 0)
+      return null
+    index = this.get_index(key)
+    if(index != -1)
+      return this.elements[index].value      
+  }
+  add(key, value) {
+    var index = this.get_index(key)
+    if(index != -1){
+      this.elements[index].value = value 
     }
-    get_index(key){
-        return this.elements.map((element,index)=>{
-            if(element.key == key){
-                console.log("encontré la key")
-                console.log(index)
-                console.log(key)
-                return index
-            }
-                
-        }) 
-    }
-    find(key) {
-        var index
-        if (this.elements.length == 0)
-            return null
-        index = this.get_index(key)
-        if(index != null)
-            return this.elements[index].value
-    }
-    add(clave, valor) {
-        var index = this.get_index(clave)
-        console.log("Acá hay un problema con el valor de retorno")
-        console.log(index)
-        if(index.length > 0){
-           this.elements[index].value = valor 
-        }
-        else{
-           this.elements.push({"key":clave,"value":valor}) 
-        }
-            
-    }
+    else{
+      if(typeof key === "string")
+        this.elements.push({"key":key,"value":value}) 
+    }   
+  }
+  remove(key){
+    var index = this.get_index(key)
+    if(index != -1){
+      this.elements.splice(index,1)
+    }  
+  }
 }
--- test/lista.js
+++ test/lista.js
@@ -3,51 +3,63 @@ const List = require("../src/lista.js")
 const expect = require("chai").expect;
 
 describe("lista de pares clave:valor",function(){
-    describe("Es una lista vacía", function(){
-        var lista = new List()  
-        it("hay cero elementos almacenados", function() {
-            assert.equal(lista.count(), 0)
-        })
-
-        it("No se encuentra clave", function() {
-            assert.isNull(lista.find("first"))
-        })
+  describe("Es una lista vacía", function(){
+    var lista = new List()  
+    it("hay cero elementos almacenados", function() {
+      assert.equal(lista.count(), 0)
     })
 
-    describe("cuando se agrega elemento a lista vacía", function() {
-        var lista = new List()
-        lista.add("first",5)
-
-        it("hay un elemento almacenado", function(){
-            assert.equal(lista.count(), 1)
-        })
-
-        it("Se recupera valor almacenado a partir de la clave", function(){
-            assert.equal(lista.find("first"), 5)
-        })
+    it("No se encuentra clave", function() {
+      assert.isNull(lista.find("first"))
     })
-    // describe("cuando se agrega un elemento duplicado a la lista", function() {
-    //     var list = new List()
-    //     list.add("primera",5)
-    //     list.add("primera",8)
-    //     it("No se aumenta el tamaño", function(){
-    //         assert.equal(list.count(), 1)
-    //     })
+  })
 
-    //     it("Se actualiza el valor", function(){
-    //         assert.equal(list.find("primera"), 8)
-    //     })
-        
-        
-    //     it("Se aumenta el tamaño", function(){
-    //         list.add("segunda",3)
-    //         assert.equal(list.count(), 2)
-    //     })
+  describe("Cuando se agrega elemento clave:valor a lista vacía", function() {
+    var lista = new List()
+    lista.add("first",5)
 
-    //     it("Se actualiza el valor", function(){
-    //         list.add("segunda",0)
-    //         assert.equal(list.find("segunda"), 0)
-    //     })
-    // })
+    it("Se recupera cantidad elementos: hay un elemento", function(){
+      assert.equal(lista.count(), 1)
+    })
+
+    it("Se recupera valor almacenado a partir de la clave", function(){
+      assert.equal(lista.find("first"), 5)
+    })
+  })
+  describe("Cuando se agrega un elemento duplicado a la lista", function() {
+    var list = new List()
+    list.add("primera",5)
+    list.add("primera",8)
+    it("No se aumenta el tamaño", function(){
+      assert.equal(list.count(), 1)
+    })
+
+    it("En su lugar, se actualiza el valor", function(){
+      assert.equal(list.find("primera"), 8)
+    })
+    
+    
+    it("Si el elemento es distinto, se aumenta el tamaño", function(){
+      list.add("segunda",3)
+      assert.equal(list.count(), 2)
+    })
+  })
+  describe("Cuando se agrega clave que no sea string", function() {
+    var list = new List()
+    list.add("primera",5)
+    list.add(6,5)
+    it("No se aumenta el tamaño", function(){
+        assert.equal(list.count(), 1)
+    })
+  })
+  describe("Si elimino elemento", function() {
+    var list = new List()
+    list.add("primera",5)
+    list.add("segunda",3)
+    list.remove("segunda")
+    it("El tamaño se reduce", function(){
+        assert.equal(list.count(), 1)
+    })
+  })
 
 })
